7b7bac306949b1685e44d1715af0ace487e9e6d8
Luís Chaves <pi.luis.f3@usp.br>
2019-11-30 20:34:55 -0300

    Add: Lists now shows their listItems on the home page

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,10 @@
       <router-link to="/about">About</router-link>
     </div> -->
     <!--Navbar deslogada-->
-    <nav
-      v-if="!userToken"
-      class="navbar navbar-expand-lg navbar-light bg-white shadow-md px-4"
-    >
+    <nav v-if="!userToken" class="navbar navbar-expand-lg navbar-light bg-white shadow-md px-4">
       <a class="navbar-brand font-semibold" href="#">Listing.io</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"/>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -47,13 +36,15 @@
         <form class="form-inline my-2">
           <div class="text-center">
             <input
-              class="bg-gray-300 mx-md-2 rounded-full py-0-35 px-3 border-2 focus-purple mr-4 placeholder-gray-700 mb-2 mb-md-0"
+              class="mx-md-2 rounded-full py-0-35 px-3 mr-4 mb-2 mb-md-0 form-control"
+              v-bind:class="{'is-invalid': incorrectLogin}"
               type="email"
               placeholder="E-mail"
               v-model="email"
             />
             <input
-              class="bg-gray-300 mx-md-2 rounded-full py-0-35 px-3 border-2 focus-purple mr-4 placeholder-gray-700"
+              class="mx-md-2 rounded-full py-0-35 px-3 mr-4 mb-2 mb-md-0 form-control"
+              v-bind:class="{'is-invalid': incorrectLogin}"
               type="password"
               placeholder="Senha"
               v-model="password"
@@ -121,6 +112,7 @@ import router from "@/router";
 export default class App extends Vue {
   private email: string = "";
   private password: string = "";
+  private incorrectLogin: boolean = false;
 
   login() {
     this.axios
@@ -132,6 +124,9 @@ export default class App extends Vue {
         Vue.prototype.userToken = response.data.token;
         router.replace("private/");
         this.email = "";
+        this.password = "";
+      }, error => {
+        this.incorrectLogin = true;
         this.password = "";
       });
   }
--- src/components/AllLists.vue
+++ src/components/AllLists.vue
@@ -3,23 +3,74 @@
     <div class="row">
       <div class="col col-md-8 col-xl-4 shadow border container p-4 my-4">
         <div class="row align-items-center">
-          <label class="col-12 col-md-5 font-weight-bold" for="title">Título:</label>
-          <input v-model="addListForm.title" class="col-12 col-md-7 form-control mb-3" type="text" id="title"/>
+          <label class="col-12 col-md-5 font-weight-bold" for="title"
+            >Título:</label
+          >
+          <input
+            v-model="addListForm.title"
+            v-bind:class="{
+              'is-invalid': incorrectAddListForm && addListForm.title === ''
+            }"
+            class="col-12 col-md-7 form-control mb-3"
+            type="text"
+            id="title"
+          />
           <label class="col-12 col-md-5 font-weight-bold" for="description"
             >Descrição:
           </label>
-          <input v-model="addListForm.description" class="col-12 col-md-7 form-control mb-3" type="text" id="description" />
+          <input
+            v-model="addListForm.description"
+            class="col-12 col-md-7 form-control mb-3"
+            type="text"
+            id="description"
+          />
           <label class="col-12 col-md-5 font-weight-bold">Cor: </label>
           <span class="col-12 col-md-7 d-flex justify-content-around mb-3">
-            <button type="button" class="rounded-pill bg-primary" v-on:click="addListForm.color = '#007bff'" style="height: 25px; width: 25px;"></button>
-            <button type="button" class="rounded-pill bg-warning" v-on:click="addListForm.color = '#ffc107'" style="height: 25px; width: 25px;"></button>
-            <button type="button" class="rounded-pill bg-success" v-on:click="addListForm.color = '#28a745'" style="height: 25px; width: 25px;"></button>
-            <button type="button" class="rounded-pill bg-danger" v-on:click="addListForm.color = '#dc3545'" style="height: 25px; width: 25px;"></button>
-            <button type="button" class="rounded-pill purple-600" v-on:click="addListForm.color = '#805ad5'" style="height: 25px; width: 25px;"></button>
-            <button type="button" class="rounded-pill teal-500" v-on:click="addListForm.color = '#38b2ac'" style="height: 25px; width: 25px;"></button>
+            <button
+              type="button"
+              class="rounded-pill bg-primary"
+              v-on:click="addListForm.color = '#007bff'"
+              style="height: 25px; width: 25px;"
+            />
+            <button
+              type="button"
+              class="rounded-pill bg-warning"
+              v-on:click="addListForm.color = '#ffc107'"
+              style="height: 25px; width: 25px;"
+            />
+            <button
+              type="button"
+              class="rounded-pill bg-success"
+              v-on:click="addListForm.color = '#28a745'"
+              style="height: 25px; width: 25px;"
+            />
+            <button
+              type="button"
+              class="rounded-pill bg-danger"
+              v-on:click="addListForm.color = '#dc3545'"
+              style="height: 25px; width: 25px;"
+            />
+            <button
+              type="button"
+              class="rounded-pill purple-600"
+              v-on:click="addListForm.color = '#805ad5'"
+              style="height: 25px; width: 25px;"
+            />
+            <button
+              type="button"
+              class="rounded-pill teal-500"
+              v-on:click="addListForm.color = '#38b2ac'"
+              style="height: 25px; width: 25px;"
+            />
           </span>
           <span class="col-12 text-center">
-            <button class="btn btn-primary" type="button" v-on:click="addList()">Adicionar lista</button>
+            <button
+              class="btn btn-primary"
+              type="button"
+              v-on:click="addList()"
+            >
+              Adicionar lista
+            </button>
           </span>
         </div>
       </div>
@@ -31,17 +82,49 @@
     <!---------->
     <!--Itens-->
     <div class="w-100 row">
+      <div v-if="allLists.length === 0" class="text-center w-100 mt-4">
+        <h3>Você não tem nenhuma lista!</h3>
+      </div>
       <!--Item-->
-      <div v-for="list in allLists" class="pb-0 col-lg-3 col-12 mt-4">
-        <div class="rounded-lg font-semibold text-md text-white px-3 py-2 d-flex align-items-center" :style= "'background-color: ' + list.color">
+      <div
+        v-for="list in allLists"
+        v-bind:key="list.id"
+        class="pb-0 col-lg-3 col-12 mt-4"
+      >
+        <div
+          class="rounded-lg font-semibold text-md text-white px-3 py-2 d-flex align-items-center"
+          v-bind:style="{ 'background-color': list.color }"
+        >
           <span class="mr-auto">{{ list.title }}</span>
-          <i class="fas fa-edit text-white pr-1"></i>
-          <i class="fas fa-times text-white"></i>
+          <i class="fas fa-edit text-white pr-1" />
+          <i class="fas fa-times text-white" />
         </div>
+        <!-- Sem items-->
+        <div
+          v-if="list.listItems.length === 0"
+          class="mt-0-2rem d-flex flex-column py-2 px-3 bg-gray-200 rounded-lg"
+        >
+          <span class="text-sm overflow-hidden"
+            >Esta lista ainda não possui nenhum item</span
+          >
+        </div>
+        <!-------------->
         <!--Checklist-->
-        <div class="mt-0-2rem d-flex flex-column py-2 px-3 bg-gray-200 rounded-lg">
-          <span v-if="!allLists.listItems" class="text-sm overflow-hidden">Esta lista ainda não possui nenhum item</span>
+        <div
+          v-for="listItem in list.listItems"
+          v-bind:key="listItem.id"
+          class="mt-0-2rem d-flex flex-column py-2 px-3 bg-gray-200 rounded-lg"
+        >
+          <span v-if="!list.listItems" class="text-sm overflow-hidden"
+            >Esta lista ainda não possui nenhum item</span
+          >
+          <span
+            class="text-sm overflow-hidden"
+            v-bind:class="{ 'crossed-text': listItem.isDone }"
+            >{{ listItem.description }}</span
+          >
         </div>
+        <!------------>
       </div>
       <!-------->
     </div>
@@ -50,7 +133,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class AllLists extends Vue {
@@ -61,22 +144,35 @@ export default class AllLists extends Vue {
     description: "",
     color: ""
   };
+  private incorrectAddListForm: boolean = false;
 
   constructor() {
     super();
     this.axios
-      .get("http://localhost:1337/list/getAll", {headers: {Authorization: 'token ' + Vue.prototype.userToken}})
+      .get("http://localhost:1337/list/getAll", {
+        headers: { Authorization: "token " + Vue.prototype.userToken }
+      })
       .then(response => {
-        for (let list of response.data) {
-          this.allLists.push(list);
-        }
+        for (let list of response.data) this.allLists.push(list);
       });
   }
 
   addList() {
+    if (this.addListForm.title === "") {
+      this.incorrectAddListForm = true;
+      return;
+    }
+
+    this.incorrectAddListForm = false;
+    this.addListForm.title = "";
+    this.addListForm.color = "";
+    this.addListForm.description = "";
+
     this.axios
-      .post("http://localhost:1337/list/create", this.addListForm, {headers: {Authorization: 'token ' + Vue.prototype.userToken}})
-      .then(response => this.allLists.push(response.data))
+      .post("http://localhost:1337/list/create", this.addListForm, {
+        headers: { Authorization: "token " + Vue.prototype.userToken }
+      })
+      .then(response => this.allLists.push(response.data));
   }
 
   print() {}
@@ -84,4 +180,8 @@ export default class AllLists extends Vue {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.crossed-text {
+  text-decoration: line-through;
+}
+</style>
