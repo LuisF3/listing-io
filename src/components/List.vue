6ec5795d577fb6ec09d858a0925e493c5abc85c7
henritarzia <henritarzia@gmail.com>
2019-12-01 22:16:54 -0300

    Template dos itens da lista

--- src/components/List.vue
+++ src/components/List.vue
@@ -3,72 +3,23 @@
     <!--Informações gerais-->
     <div class="row">
       <div class="col col-md-8 col-xl-4 shadow-sm border container p-4 my-4">
-        <h4 class="mb-3 font-weight-semibold text-secondary text-center">
-          Informações gerais
-        </h4>
+        <h4 class="mb-3 font-weight-semibold text-secondary text-center">Informações gerais</h4>
         <div class="row align-items-center pr-2">
-          <label class="col-12 col-md-4 font-weight-bold" for="title"
-            >Título:
-          </label>
-          <input
-            class="col-12 col-md-8 form-control mb-3"
-            type="text"
-            id="title"
-          />
-          <label class="col-12 col-md-4 font-weight-bold" for="listDescription"
-            >Descrição:</label
-          >
-          <input
-            class="col-12 col-md-8 form-control mb-3"
-            type="text"
-            id="listDescription"
-          />
+          <label class="col-12 col-md-4 font-weight-bold" for="title">Título:</label>
+          <input class="col-12 col-md-8 form-control mb-3" type="text" id="title"/>
+          <label class="col-12 col-md-4 font-weight-bold" for="listDescription">Descrição:</label>
+          <input class="col-12 col-md-8 form-control mb-3" type="text" id="listDescription"/>
           <label class="col-12 col-md-4 font-weight-bold">Cor: </label>
           <span class="col-12 col-md-8 d-flex justify-content-center mb-3">
-            <button
-              type="button"
-              class="rounded-pill bg-primary mr-3"
-              v-on:click="addListForm.color = '#007bff'"
-              style="height: 25px; width: 25px;"
-            />
-            <button
-              type="button"
-              class="rounded-pill bg-warning mr-3"
-              v-on:click="addListForm.color = '#ffc107'"
-              style="height: 25px; width: 25px;"
-            />
-            <button
-              type="button"
-              class="rounded-pill bg-success mr-3"
-              v-on:click="addListForm.color = '#28a745'"
-              style="height: 25px; width: 25px;"
-            />
-            <button
-              type="button"
-              class="rounded-pill bg-danger mr-3"
-              v-on:click="addListForm.color = '#dc3545'"
-              style="height: 25px; width: 25px;"
-            />
-            <button
-              type="button"
-              class="rounded-pill purple-600 mr-3"
-              v-on:click="addListForm.color = '#805ad5'"
-              style="height: 25px; width: 25px;"
-            />
-            <button
-              type="button"
-              class="rounded-pill teal-500"
-              v-on:click="addListForm.color = '#38b2ac'"
-              style="height: 25px; width: 25px;"
-            />
+            <button type="button" class="rounded-pill bg-primary mr-3" v-on:click="addListForm.color = '#007bff'" style="height: 25px; width: 25px;"/>
+            <button type="button" class="rounded-pill bg-warning mr-3" v-on:click="addListForm.color = '#ffc107'" style="height: 25px; width: 25px;"/>
+            <button type="button" class="rounded-pill bg-success mr-3" v-on:click="addListForm.color = '#28a745'" style="height: 25px; width: 25px;"/>
+            <button type="button" class="rounded-pill bg-danger mr-3" v-on:click="addListForm.color = '#dc3545'" style="height: 25px; width: 25px;"/>
+            <button type="button" class="rounded-pill purple-600 mr-3" v-on:click="addListForm.color = '#805ad5'" style="height: 25px; width: 25px;"/>
+            <button type="button" class="rounded-pill teal-500" v-on:click="addListForm.color = '#38b2ac'" style="height: 25px; width: 25px;"/>
           </span>
           <span class="col-12 text-center">
-            <button
-              class="btn btn-primary rounded-pill mt-2 font-weight-semibold"
-              type="button"
-            >
-              Salvar alterações
-            </button>
+            <button class="btn btn-primary rounded-pill mt-2 font-weight-semibold" type="button">Salvar alterações</button>
           </span>
         </div>
       </div>
@@ -82,46 +33,25 @@
           Nova tarefa
         </h4>
         <div class="row align-items-center pr-2">
-          <label class="col-12 col-md-4 font-weight-bold" for="description"
-            >Descrição:</label
-          >
-          <input
-            class="col-12 col-md-8 form-control mb-3"
-            type="text"
-            id="description"
-          />
-          <label class="col-12 col-md-4 font-weight-bold" for="type"
-            >Categoria:</label
-          >
-          <select
-            class="col-12 col-md-8 form-control mb-3 text-center"
-            id="type"
-          >
+          <label class="col-12 col-md-4 font-weight-bold" for="description">Descrição:</label>
+          <input class="col-12 col-md-8 form-control mb-3" type="text" id="description"/>
+          <label class="col-12 col-md-4 font-weight-bold" for="type">Categoria:</label>
+          <select class="col-12 col-md-8 form-control mb-3 text-center" id="type">
             <option value="lembrete">Lembrete</option>
             <option value="evento">Evento</option>
             <option value="meta">Meta</option>
           </select>
-          <label class="col-12 col-md-4 font-weight-bold" for="createIsDone"
-            >Concluído:</label
-          >
-          <select
-            class="col-12 col-md-8 form-control mb-3 text-center"
-            id="createIsDone"
-          >
+          <label class="col-12 col-md-4 font-weight-bold" for="createIsDone">Concluído:</label>
+          <select class="col-12 col-md-8 form-control mb-3 text-center" id="createIsDone">
             <option value="true">Sim</option>
             <option value="false">Não</option>
           </select>
           <span class="col-12 text-center">
-            <button
-              class="btn btn-primary rounded-pill mt-2 font-weight-semibold"
-              type="button"
-            >
-              Adicionar
-            </button>
+            <button class="btn btn-primary rounded-pill mt-2 font-weight-semibold" type="button">Adicionar</button>
           </span>
         </div>
       </div>
-      <!------------------------------->
+      <!------------------------------>
     </div>
 
     <!--Lista de itens-->
@@ -131,121 +61,57 @@
       </h4>
 
       <div>
-        <select
-            class="col-12 text-center bg-primary rounded text-white font-weight-semibold py-2"
-            id="filter"
-          >
-            <option value="lembrete">Todas</option>
-            <option value="lembrete">Concluídas</option>
-            <option value="lembrete">Em andamento</option>
-            <option value="lembrete">Lembretes</option>
-            <option value="evento">Eventos</option>
-            <option value="meta">Metas</option>
-          </select>
+        <select class="col-12 text-center bg-primary rounded text-white font-weight-semibold py-2" id="filter">
+          <option value="lembrete">Todas</option>
+          <option value="lembrete">Concluídas</option>
+          <option value="lembrete">Em andamento</option>
+          <option value="lembrete">Lembretes</option>
+          <option value="evento">Eventos</option>
+          <option value="meta">Metas</option>
+        </select>
       </div>
 
-      <div class="container-fluid pb-2">
+      <div class="container-fluid pb-2" v-for="item in listItems" v-bind:key="item.id">
         <div class="bg-light my-2 py-2 row rounded">
+          <!--Descricao-->
           <div class="col-5 text-truncate">
-            <input type="checkbox" />
-            <span class="ml-2 hover-primary"
-              >Descrição de lembrete em andamento</span
-            >
+            <input type="checkbox" v-model="item.isDone"/>
+            <span class="ml-2 hover-primary" v-if="!item.isDone">{{ item.description }}</span>
+            <span class="ml-2 text-secondary" style="text-decoration: line-through;" v-if="item.isDone">{{ item.description }}</span>
           </div>
-          <div class="col-6"></div>
-          <div class="col-1">
-            <button><i class="fa-trash" /></button>
+          <!------------>
+
+          <!--Lembrete-->
+          <div class="col-6" v-if="item.type == lembrete"></div>
+          <!------------>
+
+          <!----Evento---->
+          <div class="col-6" v-if="item.type == evento">
+            <input type="date" class="no-outline bg-light" v-model="item.date"/>
           </div>
-        </div>
-        <div class="bg-light my-2 py-2 row rounded">
-          <div class="col-5 text-truncate">
-            <input type="checkbox" />
-            <span
-              class="ml-2 text-secondary"
-              style="text-decoration: line-through;"
-              >Descrição de lembrete concluído</span
-            >
-          </div>
-          <div class="col-6"></div>
-          <div class="col-1">
-            <button><i class="fa-trash" /></button>
-          </div>
-        </div>
-        <div class="bg-light my-2 py-2 row rounded">
-          <div class="col-5 text-truncate">
-            <input type="checkbox" />
-            <span class="ml-2 hover-primary"
-              >Descrição de evento em andamento</span
-            >
-          </div>
-          <div class="col-6 text-center">
-            <input type="date" class="no-outline bg-light" />
-          </div>
-          <div class="col-1">
-            <button><i class="fa-trash" /></button>
-          </div>
-        </div>
-        <div class="bg-light my-2 py-2 row rounded">
-          <div class="col-5 text-truncate">
-            <input type="checkbox" />
-            <span
-              class="ml-2 text-secondary"
-              style="text-decoration: line-through;"
-              >Descrição de evento concluído</span
-            >
-          </div>
-          <div class="col-6 text-center">
-            <input type="date" class="no-outline bg-light" />
-          </div>
-          <div class="col-1">
-            <button><i class="fa-trash" /></button>
-          </div>
-        </div>
-        <div class="bg-light my-2 py-2 row rounded">
-          <div class="col-5 text-truncate">
-            <input type="checkbox" />
-            <span class="ml-2 hover-primary"
-              >Descrição de meta em andamento</span
-            >
-          </div>
-          <div class="col-6">
-            <div class="row container">
-              <button class="bg-primary" />
-              <input type="number" class="border" />
+          <!-------------->
+
+          <!----Meta---->
+          <div class="col-6" v-if="item.type == meta">
+            <div class="container">
+              <button type="button" class="bg-primary" v-on:click="item.increase()"><i class="fas fa-plus"></i></button>
+              <input type="number" class="border" v-model="item.currentQtd"/>
               <span class="px-2">/</span>
-              <input type="number" class="border" />
-              <button class="bg-primary" />
+              <input type="number" class="border" v-model="item.maxQtd"/>
+              <button type="button" class="bg-primary" v-on:click="item.decrease()"><i class="fas fa-minus"></i></button>
             </div>
           </div>
+          <!------------>
+
+          <!----Remover---->
           <div class="col-1">
-            <button><i class="fa-trash" /></button>
+            <button v-on:click="item.remove()"><i class="fas fa-trash"></i></button>
           </div>
-        </div>
-        <div class="bg-light my-2 py-2 row rounded">
-          <div class="col-5 text-truncate">
-            <input type="checkbox" id="isDone" />
-            <span
-              class="ml-2 text-secondary"
-              style="text-decoration: line-through;"
-              >Descrição de meta concluída</span
-            >
-          </div>
-          <div class="col-6">
-            <div class="row container">
-              <button type="button" class="bg-primary" />
-              <input type="number" class="border" />
-              <span class="px-2">/</span>
-              <input type="number" class="border" />
-              <button type="button" class="bg-primary" />
-            </div>
-          </div>
-          <div class="col-1">
-            <button><i class="fa-trash" /></button>
-          </div>
+          <!-------------->
         </div>
       </div>
-    </div>
-    <!------------------>
+    </div>          
+  <!------------------>
   </div>
 </template>
 
